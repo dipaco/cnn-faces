7c3776115f6b4f3019f088354e0231a11b8eb2a7
lfsalazarz <lfsalazarz@unal.edu.co>
2016-10-10 19:09:03 -0500

    Add files via upload

--- data_batch_bin.py
+++ data_batch_bin.py
@@ -1,6 +1,18 @@
 import numpy as np
 from PIL import Image
 
+def progressB(step, total, bar_len, info = '', symb_f = '\u2588', symb_e = '-'):
+    import sys
+    from math import ceil
+
+    progress = (step/total) * 100.0
+    step_bar = int(ceil(bar_len * (step / total)))
+    bar = (symb_f * step_bar) + (symb_e * (bar_len - step_bar))
+
+    sys.stdout.write('\r|%s| %.1f%s %s'%(bar,progress,'%',info))
+    sys.stdout.flush()
+	
+	
 def binary_file(bin_filename,ls_image,ls_label, size = (32,32), dump = 0):
     """
     https://www.cs.toronto.edu/~kriz/cifar.html
@@ -28,9 +40,11 @@ def binary_file(bin_filename,ls_image,ls_label, size = (32,32), dump = 0):
      are the red channel values of the first row of the image. 
     """
     ls = []
+
     for k,fn in enumerate(ls_image):
-        i = Image.open(fn)
-        
+        progressB(k, len(ls_image), bar_len = 60, info = fn)
+	
+        i = Image.open(fn)        
         i.thumbnail(size, Image.ANTIALIAS)        
         padding = Image.new('RGBA', size, (0, 0, 0, 0))
         padding.paste(i,(int((size[0] - i.size[0]) / 2), int((size[1] - i.size[1]) / 2)))
@@ -42,7 +56,7 @@ def binary_file(bin_filename,ls_image,ls_label, size = (32,32), dump = 0):
         g = arr[:,:,0].flatten()
         b = arr[:,:,0].flatten()
         r = np.array(list(lbl) + list(r) + list(g) + list(b), np.uint8)
-        ls.append(r)
+        ls.append(r)        
 
     output = np.array(ls)
 
@@ -63,6 +77,3 @@ def read_binary_file(bin_filename, dump = 0):
         return unpickle(bin_filename)
     else:
         return np.fromfile(bin_filename,dtype = np.uint8)
-        
-    
-
