74975665e63013d3be5358c9e2a5458bb3bf3696
lfsalazarz <lfsalazarz@unal.edu.co>
2016-10-15 21:49:03 -0500

    Add files via upload

--- data_batch_bin.py
+++ data_batch_bin.py
@@ -12,7 +12,8 @@ def progressB(step, total, bar_len, info = '', symb_f = '\u2588', symb_e = '-'):
     sys.stdout.write('\r|%s| %.1f%s %s'%(bar,progress,'%',info))
     sys.stdout.flush()
 	
-	
+
+
 def binary_file(bin_filename,ls_image,ls_label, size = (32,32), dump = 0):
     """
     https://www.cs.toronto.edu/~kriz/cifar.html
@@ -65,6 +66,8 @@ def binary_file(bin_filename,ls_image,ls_label, size = (32,32), dump = 0):
     else:
         output.tofile(bin_filename)
 
+def binary_file(bin_filename,image):
+    pass
 
 def read_binary_file(bin_filename, dump = 0):
     if dump:
@@ -77,3 +80,59 @@ def read_binary_file(bin_filename, dump = 0):
         return unpickle(bin_filename)
     else:
         return np.fromfile(bin_filename,dtype = np.uint8)
+
+
+def arr_image(filename, height = 32 ,width = 32, hwp = False):
+    # Image:
+    #   +----+----+----+----+
+    #   | 0  | 1  | 2  | 3  |
+    #   +----+----+----+----+
+    #   | 4  | 5  | 6  | 7  |
+    #   +----+----+----+----+
+    # 
+    with Image.open(filename) as im:
+        imgwidth, imgheight = im.size
+        grid = []
+        if hwp:
+            height = width = int((imgwidth + imgheight) / 8)
+        for h in range(0, imgheight, height):
+            for w in range(0,imgwidth,width):
+                grid.append(im.crop((w, h, w+width, h+height)))
+        return grid       
+
+def binary_file(bin_filename,im_filename, size = (32,32), dump = 0):
+    ls = []
+    ls_images = arr_image(im_filename, hwp = True)
+    ls_label = [0 for i in range(0,len(ls_images))]
+    
+    for k,im in enumerate(ls_images): 
+        im.thumbnail(size, Image.ANTIALIAS)        
+        padding = Image.new('RGBA', size, (0, 0, 0, 0))
+        padding.paste(im,(int((size[0] - im.size[0]) / 2), int((size[1] - im.size[1]) / 2)))
+        im2 = padding        
+        
+        arr = np.array(im2)
+        lbl = [ls_label[k]]
+        r = arr[:,:,0].flatten()
+        g = arr[:,:,0].flatten()
+        b = arr[:,:,0].flatten()
+        r = np.array(list(lbl) + list(r) + list(g) + list(b), np.uint8)
+        ls.append(r)   
+
+        progressB(k+1, len(ls_images), bar_len = 60, info = 'im_%d'%k)
+
+    output = np.array(ls)
+    if dump:
+        output.dump(bin_filename)
+    else:
+        output.tofile(bin_filename)
+
+        
+if __name__ == '__main__':
+    import sys    
+    if len(sys.argv) > 2:
+        bin_filename = sys.argv[1]
+        im_filename = sys.argv[2]
+        binary_file(bin_filename,im_filename)
+    
+   
